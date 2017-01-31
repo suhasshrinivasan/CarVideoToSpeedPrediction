754641c8093157725f6a38984c348058552fde4e
ethanp56 <ethanp56@yahoo.com>
2017-01-31 00:05:01 -0600

    fixed bug in generating visualizations

--- gen_cool_viz.py
+++ gen_cool_viz.py
@@ -1,7 +1,7 @@
 print('echo "Starting to generate cool visualizations!"')
-print('mkdir visualization')
+print('mkdir viz')
 for i in range(8616):
     print('./darknet detector test cfg/coco.data cfg/yolo.cfg yolo.weights drive/frame%d.jpg' % i)
-    print('mv predictions.jpg visualization/frame%d.jpg' % i)
-print('ffmpeg -i frame%d.png -f mp4 -vcodec libx264 -pix_fmt yuv420p test.mp4')
+    print('mv predictions.jpg viz/frame%d.jpg' % i)
+print('ffmpeg -pix_fmt yuv420p -framerate 24 -i viz/frame%d.jpg -f mp4 -vcodec h264 cool_viz.mp4')
 print('echo "Done generating cool visualizations!""')
